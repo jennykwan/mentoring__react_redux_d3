e53e0be49dad582f860a556cc9b4e8cd823de26b
Alyssa Kwan <me@alyssackwan.name>
2017-01-10 06:07:44 -0800

    strip out for exercise 1

--- client/src/exercise-2-d3-only/services/rsvps.js
+++
@@ -1,9 +0,0 @@
-export default (callback) => {
-  const protocol = (location.protocol === 'https:') ? 'wss:' : 'ws:';
-  const url = `${protocol}//${location.host}/rsvps`;
-
-  const ws = new WebSocket(url);
-  ws.addEventListener('message', (message, flags) => {
-    callback(JSON.parse(message.data));
-  });
-};
--- client/src/index.js
+++ client/src/index.js
@@ -2,19 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Exercise1App from './exercise-1-react-only/App';
-import exercise2App from './exercise-2-d3-only/app';
-import Exercise3App from './exercise-3-react-and-d3/App';
 
 import './index.css';
 
 ReactDOM.render(
   <Exercise1App />,
   document.getElementById('exercise-1-react-only')
-);
-
-exercise2App('#exercise-2-d3-only');
-
-ReactDOM.render(
-  <Exercise3App />,
-  document.getElementById('exercise-3-react-and-d3')
 );
